c78e3f9ea7a67f4d63b53bd1135ae6f0e0781756
15914115336@163.com <qijinliang123>
2019-11-05 14:41:20 +0800

    添加代码注释

--- SwiftUI-MVVM-Jason/SwiftUI-MVVM-Jason/ContentView.swift
+++ SwiftUI-MVVM-Jason/SwiftUI-MVVM-Jason/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 let apiUrl = "https://api.letsbuildthatapp.com/static/courses.json"
 
+//定义模型
 struct Course: Identifiable, Decodable{
     let id = UUID()
     let name: String
@@ -18,20 +19,22 @@ struct Course: Identifiable, Decodable{
 
 
 class CoursesViewModel: ObservableObject {
-    
+    //测试数据
     @Published var courses: [Course] = [
         .init(name: "Course 1",price: 0),
         .init(name: "Course 2",price: 0),
         .init(name: "Course 3",price: 0)
     ]
-    
+    //网络请求方法
     func fetchCourses() {
+        //判断地址是否请求成功
         guard let url = URL(string: apiUrl) else { return }
         
         URLSession.shared.dataTask(with: url) {(data, resp, err) in
             
             DispatchQueue.main.async {
                 do {
+                    //模型转换
                     self.courses = try JSONDecoder().decode([Course].self, from:data!)
                 } catch {
                     
@@ -44,13 +47,17 @@ class CoursesViewModel: ObservableObject {
 
 
 struct ContentView: View {
-    
+    //加载模型
     @ObservedObject var coursesVM = CoursesViewModel()
     
     var body: some View {
+        //设置导航栏
         NavigationView {
+            //添加滚动视图
             ScrollView {
+                //列表文字左边对齐
                 VStack(alignment: .leading) {
+                    //遍历模型
                     ForEach(coursesVM.courses) { course in
                         HStack {
                             Text(course.name)
@@ -65,14 +72,16 @@ struct ContentView: View {
                 }.padding(.horizontal, 22)
                     .padding(.top, 8)
                 
-                
+            //列表的标题
             }.navigationBarTitle("Courses")
+            //导航栏按钮的点击事件
             .navigationBarItems(trailing: Button(action: {
                 print("Fetching json data")
-                
+                //请求网络
                 self.coursesVM.fetchCourses()
                 
             }, label: {
+                //导航栏的标题
                 Text("Fetch Courses")
             }))
         }
