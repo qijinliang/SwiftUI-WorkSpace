13ee70332078dfde18ea057f0a7b7f510615e276
15914115336@163.com <qijinliang123>
2019-11-06 15:10:01 +0800

    完成布局，添加模型布局

--- SwiftUI-Layout/SwiftUI-Layout/ContentView.swift
+++ SwiftUI-Layout/SwiftUI-Layout/ContentView.swift
@@ -10,14 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        VStack{
-            Text("Hello World").font(.system(size: 24))
-                .padding(.horizontal,12)
-                .padding(.vertical,8)
-        }.background(Color.blue)
+        ZStack {
+           Color.green.edgesIgnoringSafeArea(.all)
+           FrontView()
+        }
     }
 }
+
+struct FrontView: View {
+    var body: some View {
+        VStack (spacing: 12) {
+            HStack {
+                Text("Hello World")
+                    .font(.system(size: 24))
+                    .padding(.horizontal,12)
+                    .padding(.vertical,8)
+                    .background(Color.red)
+                Spacer().frame(height: 10)
+                        .background(Color.green)
+            }.background(Color.blue)
+            
+            Spacer().frame(width: 10)
+                    .background(Color.green)
+            HStack{
+                Spacer().frame(height: 10)
+                        .background(Color.green)
+                Text("Hello World")
+                    .font(.system(size: 24))
+                    .padding(.horizontal,12)
+                    .padding(.vertical,8)
+                    .background(Color.red)
+            }.background(Color.blue)
+            
+        }.background(Color.yellow)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ SwiftUI-Modifiers/SwiftUI-Modifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Modifiers
+//
+//  Created by Jerry on 2019/11/6.
+//  Copyright © 2019 QijinLiang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
