38827bd4c8d4d47e98e292d41b1a4c41b39bd0e2
15914115336@163.com <qijinliang123>
2019-11-07 10:18:09 +0800

    完成计算器页面的搭建

--- SwiftUI-Calculator/SwiftUI-Calculator/ContentView.swift
+++ SwiftUI-Calculator/SwiftUI-Calculator/ContentView.swift
@@ -8,33 +8,56 @@
 
 import SwiftUI
 
-struct CalculatorButton: Identifiable {
+struct CalculatorButton: Identifiable,Hashable {
     let id = UUID()
     let title: String
-    var color = Color(red: 0.2, green: 0.2, blue: 0.2)
+    var color: Color = .darkGray
+}
+
+extension Color {
+    static let lightGray = Color(red: 0.6, green: 0.6, blue: 0.6)
+    static let darkGray = Color(red: 0.2, green: 0.2, blue: 0.2)
 }
 
 struct CalculatorView: View {
     
-    let buttons: [CalculatorButton] = [
-        .init(title: "1"),
+    let buttons: [[CalculatorButton]] = [
+        [.init(title: "AC", color: .lightGray),
+        .init(title: "±", color: .lightGray),
+        .init(title: "%", color: .lightGray),
+        .init(title: "÷", color: Color.orange)],
+        [.init(title: "7"),
+        .init(title: "8"),
+        .init(title: "9"),
+        .init(title: "X", color: Color.orange)],
+        [.init(title: "4"),
+        .init(title: "5"),
+        .init(title: "6"),
+        .init(title: "-", color: Color.orange)],
+        [.init(title: "1"),
         .init(title: "2"),
         .init(title: "3"),
-        .init(title: "+", color: Color.orange)
+        .init(title: "+", color: Color.orange)],
+        [.init(title: "0"),
+        .init(title: "."),
+        .init(title: "=", color: Color.orange)]
+        
     ]
     
     let spacing: CGFloat = 12
     
     var body: some View {
-        VStack {
-            Spacer()
-            
-            GeometryReader { geometry in
-                CalculatorButtonsRow(screenWidth: geometry.size.width, spacing: self.spacing, buttons: self.buttons)
-            }
-            
-            Spacer()
-        }.background(Color.black)
+        GeometryReader { geometry in
+            ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
+                VStack(spacing: self.spacing) {
+                    Spacer()
+                    ForEach(self.buttons, id: \.self) { row in
+                            CalculatorButtonsRow(screenWidth: geometry.size.width, spacing: self.spacing, buttons: row)
+                        }
+                    }
+                }
+        }
     }
 }
 
@@ -44,13 +67,19 @@ struct CalculatorButtonsRow: View {
     let spacing: CGFloat
     let buttons: [CalculatorButton]
     
+    func getButtonWidth(title: String) -> CGFloat {
+        return title != "0" ?
+        (self.screenWidth - self.spacing * 5) / 4 :
+        (self.screenWidth - self.spacing * 5) / 4 * 2 + self.spacing
+    }
+    
     var body: some View {
         HStack(spacing: self.spacing) {
            ForEach(self.buttons) {button in
                Text(button.title)
                 .font(.system(size: 28))
                 .foregroundColor(.white)
-                .frame(width: (self.screenWidth - self.spacing * 5) / 4, height: (self.screenWidth - self.spacing * 5) / 4)
+                .frame(width: self.getButtonWidth(title: button.title), height: (self.screenWidth - self.spacing * 5) / 4)
                 .background(button.color)
                 .cornerRadius(100)
                }
