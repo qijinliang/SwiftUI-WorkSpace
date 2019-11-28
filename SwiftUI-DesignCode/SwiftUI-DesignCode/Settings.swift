d45e569140ab1d099d60c0ebc4a92c6cea361749
“qijinliang” <15914115336.com>
2019-11-28 14:34:07 +0800

    汉化

--- SwiftUI-DesignCode/SwiftUI-DesignCode/Settings.swift
+++ SwiftUI-DesignCode/SwiftUI-DesignCode/Settings.swift
@@ -9,13 +9,50 @@
 import SwiftUI
 
 struct Settings: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
-    }
+
+   @State var receive = false
+   @State var number = 1
+   @State var selection = 1
+   @State var date = Date()
+   @State var email = ""
+   @State var submit = false
+
+   var body: some View {
+      NavigationView {
+         Form {
+            Toggle(isOn: $receive) {
+               Text("收到通知")
+            }
+            Stepper(value: $number, in: 1...10) {
+               Text("\(number) 通知\(number > 1 ? "s" : "") 这周")
+            }
+            Picker(selection: $selection, label: Text("最喜欢的课程")) {
+               Text("SwiftUI").tag(1)
+               Text("React").tag(2)
+            }
+            DatePicker(selection: $date, label: {
+               Text("日期")
+            })
+            Section(header: Text("邮箱")) {
+               TextField("你的邮箱: ", text: $email)
+                  .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            Button(action: { self.submit.toggle() }) {
+               Text("提交")
+            }
+            .alert(isPresented: $submit, content: {
+               Alert(title: Text("谢谢"), message: Text("邮箱: \(email)"))
+            })
+         }
+         .navigationBarTitle("设置")
+      }
+   }
 }
 
+#if DEBUG
 struct Settings_Previews: PreviewProvider {
-    static var previews: some View {
-        Settings()
-    }
+   static var previews: some View {
+      Settings()
+   }
 }
+#endif
--- SwiftUI-DesignCode/SwiftUI-DesignCode/UpdateList.swift
+++ SwiftUI-DesignCode/SwiftUI-DesignCode/UpdateList.swift
@@ -87,11 +87,11 @@ struct Update: Identifiable {
 }
 
 let updateData = [
-    Update(image: "Illustration1", title: "SwiftUI", text: "Learn how to build custom views and controls in SwiftUI with advanced composition, layout, graphics, and animation. See a demo of a high performance, animatable control and watch it made step by step in code. Gain a deeper understanding of the layout system of SwiftUI.", date: "JUN 26"),
-    Update(image: "Illustration2", title: "Framer X", text: "Learn how to build custom views and controls in SwiftUI with advanced composition, layout, graphics, and animation. See a demo of a high performance, animatable control and watch it made step by step in code. Gain a deeper understanding of the layout system of SwiftUI.", date: "JUN 11"),
-    Update(image: "Illustration3", title: "CSS Layout", text: "Learn how to combine CSS Grid, Flexbox, animations and responsive design to create a beautiful prototype in CodePen.", date: "MAY 26"),
-    Update(image: "Illustration4", title: "React Native Part 2", text: "Learn how to implement gestures, Lottie animations and Firebase login.", date: "MAY 15"),
-    Update(image: "Certificate1", title: "Unity", text: "Unity course teaching basics, C#, assets, level design and gameplay", date: "MAR 19"),
-    Update(image: "Certificate2", title: "React Native for Designers", text: "Build your own iOS and Android app with custom animations, Redux and API data.", date: "FEB 14"),
-    Update(image: "Certificate3", title: "Vue.js", text: "Make a dashboard web-app with a complete login system, dark mode, and animated charts for your data.", date: "JAN 23")
+    Update(image: "Illustration1", title: "SwiftUI", text: "了解如何使用高级组合、布局、图形和动画在SwiftUI中构建自定义视图和控件。看看一个高性能的演示，动画控制和观看它一步一步在代码。对SwiftUI的布局系统有了更深入的了解。", date: "JUN 26"),
+    Update(image: "Illustration2", title: "Framer X", text: "了解如何使用高级组合、布局、图形和动画在SwiftUI中构建自定义视图和控件。看看一个高性能的演示，动画控制和观看它一步一步在代码。对SwiftUI的布局系统有了更深入的了解。", date: "JUN 11"),
+    Update(image: "Illustration3", title: "CSS Layout", text: "学习如何结合CSS网格，Flexbox，动画和响应设计在CodePen创建一个美丽的原型。", date: "MAY 26"),
+    Update(image: "Illustration4", title: "React Native Part 2", text: "学习如何实现手势，Lottie动画和Firebase登录。", date: "MAY 15"),
+    Update(image: "Certificate1", title: "Unity", text: "统一课程教学基础，c#，资产，水平设计和游戏", date: "MAR 19"),
+    Update(image: "Certificate2", title: "React Native for Designers", text: "使用自定义动画、Redux和API数据构建自己的iOS和Android应用程", date: "FEB 14"),
+    Update(image: "Certificate3", title: "Vue.js", text: "制作一个仪表板web应用程序，包括完整的登录系统、暗模式和动态图表。", date: "JAN 23")
 ]
