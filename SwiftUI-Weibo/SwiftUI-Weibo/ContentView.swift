293be8d08fd56b1a04bacb2dae7727b16a6b9bd6
醉看红尘这场梦 <15914115336@163.com>
2020-02-23 14:36:52 +0800

    创建微博项目

---
+++ SwiftUI-Weibo/SwiftUI-Weibo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weibo
+//
+//  Created by qjinliang on 2020/2/23.
+//  Copyright © 2020 qjinliang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
