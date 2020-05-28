4ded5ef7690b800d672ba64c2e570266ce302e6c
qijinliang <15914115336@163.com>
2020-05-28 14:02:27 +0800

    更新提交

---
+++ SwiftUI_JIKE-master/SwiftUI_JIKE/ContentViewSimple.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_JIKE
+//
+//  Created by g on 2019/12/8.
+//  Copyright © 2019 g. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentViewSimple: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentViewSimple_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentViewSimple()
+    }
+}
