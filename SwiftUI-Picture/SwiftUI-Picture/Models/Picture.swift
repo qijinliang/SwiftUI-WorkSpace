6e45701210548c66abfd7047199192161772924c
qijinliang <15914115336@163.com>
2020-06-14 19:45:12 +0800

    添加图片选择

---
+++ SwiftUI-Picture/SwiftUI-Picture/Models/Picture.swift
@@ -0,0 +1,23 @@
+//
+//  Picture.swift
+//  SwiftUI-Picture
+//
+//  Created by qjinliang on 2020/6/14.
+//  Copyright © 2020 醉看红尘这场梦. All rights reserved.
+//
+
+import Foundation
+
+struct Picture: Identifiable {
+    let id = UUID()
+    var imageName: String
+    
+    static func data() -> [Picture] {
+        return [
+            .init(imageName: "1"),
+            .init(imageName: "2"),
+            .init(imageName: "3"),
+            .init(imageName: "4"),
+        ]
+    }
+}
