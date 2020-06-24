da37045960fbf9e7dc5de51d8c80fe6726ab75b2
qijinliang <15914115336@163.com>
2020-06-24 10:27:24 +0800

    添加SwiftUI仿App Store

---
+++ SwiftUI-AppStore/SwiftUI-AppStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-AppStore
+//
+//  Created by qjinliang on 2020/6/24.
+//  Copyright © 2020 醉看红尘这场梦. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
