1ea220d98c671f75d935846e30257a62be4bf3f3
醉看红尘这场梦 <15914115336@163.com>
2021-03-11 22:37:02 +0800

    矩形和Textfield

---
+++ SwiftUI-Example/SwiftUI-Example/TextField/TextFieldSwiftUIView.swift
@@ -0,0 +1,63 @@
+//
+//  TextFieldSwiftUIView.swift
+//  SwiftUI-Example
+//
+//  Created by Cheney on 2021/3/11.
+//
+
+import SwiftUI
+
+struct TextFieldSwiftUIView: View {
+    @State var email = ""
+    @State var password = ""
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "person.crop.circle.fill")
+                    .foregroundColor(Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1.0)))
+                    .frame(width: 44, height: 44)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                    .padding(.leading)
+                
+                TextField("Your Emial".uppercased(), text: $email)
+                    .keyboardType(.emailAddress)
+                    .font(.subheadline)
+                    .padding(.leading)
+                    .frame(height: 44)
+            }
+            
+            Divider().padding(.leading, 80)
+            
+            HStack {
+                Image(systemName: "lock.fill")
+                    .foregroundColor(Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1.0)))
+                    .frame(width: 44, height: 44)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                    .padding(.leading)
+                
+                SecureField("Password".uppercased(), text: $password)
+                    .keyboardType(.default)
+                    .font(.subheadline)
+                    .padding(.leading)
+                    .frame(height: 44)
+            }
+        }
+        .frame(height: 136)
+        .frame(maxWidth: .infinity)
+        .background(Color(#colorLiteral(red: 0.1411764705882353, green: 0.396078431372549, blue: 0.5647058823529412, alpha: 1.0)))
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color.black.opacity(0.15), radius: 20, x: 0, y: 20)
+        .padding(.horizontal)
+        .offset(y: 200)
+    }
+}
+
+struct TextFieldSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldSwiftUIView().preferredColorScheme(.dark)
+    }
+}
---
+++ SwiftUI-Example/SwiftUI-Example/mathematics/RectangleSwiftUIView.swift
@@ -0,0 +1,52 @@
+//
+//  RectangleSwiftUIView.swift
+//  SwiftUI-Example
+//
+//  Created by Cheney on 2021/3/11.
+//
+
+import SwiftUI
+
+
+struct RectangleSwiftUIView: View {
+    
+    var body: some View {
+        PolygonShape(sides: 3).stroke(Color(#colorLiteral(red: 0.8549019607843137, green: 0.25098039215686274, blue: 0.47843137254901963, alpha: 1.0)), lineWidth: 3)
+        PolygonShape(sides: 4).stroke(Color(#colorLiteral(red: 0.9411764705882353, green: 0.4980392156862745, blue: 0.35294117647058826, alpha: 1.0)), lineWidth: 4)
+        
+    }
+}
+
+struct PolygonShape: Shape {
+    
+    var sides: Int
+    
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let h = Double(min(rect.size.width, rect.size.height)) / 2.0
+        
+        let c = CGPoint(x: rect.size.width / 2.0, y: rect.size.height / 2.0)
+        
+        var path = Path()
+        
+        for i in 0..<sides {
+            let angle = (Double(i) * (360.0 / Double(sides))) * Double.pi / 180
+            let pt = CGPoint(x: c.x + CGFloat(cos(angle) * h), y: c.y + CGFloat(sin(angle) * h))
+            
+            if i == 0 {
+                path.move(to: pt)
+            }else {
+                path.addLine(to: pt)
+            }
+        }
+        path.closeSubpath()
+        
+        return path
+    }
+}
+struct RectangleSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        RectangleSwiftUIView().preferredColorScheme(.dark)
+    }
+}
