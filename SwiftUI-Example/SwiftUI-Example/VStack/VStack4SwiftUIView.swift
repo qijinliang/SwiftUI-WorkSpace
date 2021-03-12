9e9488174c23a6e876468167e59a5dd6a3c773c8
醉看红尘这场梦 <15914115336@163.com>
2021-03-12 20:13:22 +0800

    修复名字错误问题

--- SwiftUI-Example/SwiftUI-Example/VStack/VStack4SwiftUIView.swift
+++ SwiftUI-Example/SwiftUI-Example/VStack/VStack4SwiftUIView.swift
@@ -1,20 +1,20 @@
 //
-//  VStacj4SwiftUIView.swift
+//  VStack4SwiftUIView.swift
 //  SwiftUI-Example
 //
-//  Created by Cheney on 2021/3/10.
+//  Created by Cheney on 2021/3/12.
 //
 
 import SwiftUI
 
-struct VStacj4SwiftUIView: View {
+struct VStack4SwiftUIView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct VStacj4SwiftUIView_Previews: PreviewProvider {
+struct VStack4SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        VStacj4SwiftUIView()
+        VStack4SwiftUIView()
     }
 }
