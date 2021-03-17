f4231012354dc1fb26ad74a189eb25970ecf13f7
醉看红尘这场梦 <15914115336@163.com>
2021-03-17 18:15:55 +0800

    HStack示例

---
+++ SwiftUI-Example/SwiftUI-Example/HStack/HStack2SwiftUIView.swift
@@ -0,0 +1,58 @@
+//
+//  HStack2SwiftUIView.swift
+//  SwiftUI-Example
+//
+//  Created by Cheney on 2021/3/17.
+//
+
+import SwiftUI
+
+struct HStack2SwiftUIView: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            Text("HStack")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            Text("Spacing")
+                .font(.largeTitle)
+                .foregroundColor(.gray)
+            
+            VStack() {
+                Text("The HStack initalizer allows you to set the spacing between all the views inside the HStack.")
+                    .font(.title)
+                    .foregroundColor(.black)
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.orange)
+            
+            Text("Default Spacing")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            
+            HStack() {
+                Image.init(systemName: "1.circle")
+                Image.init(systemName: "2.circle")
+                Image.init(systemName: "3.circle")
+            }
+            .font(.largeTitle)
+            
+            Text("Spacing: 100")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            
+            HStack(spacing: 100) {
+                Image.init(systemName: "1.circle")
+                Image.init(systemName: "2.circle")
+                Image.init(systemName: "3.circle")
+            }
+            .font(.largeTitle)
+        }
+    }
+}
+
+struct HStack2SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack2SwiftUIView().preferredColorScheme(.dark)
+    }
+}
---
+++ SwiftUI-Example/SwiftUI-Example/HStack/HStack3SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HStack3SwiftUIView.swift
+//  SwiftUI-Example
+//
+//  Created by Cheney on 2021/3/17.
+//
+
+import SwiftUI
+
+struct HStack3SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HStack3SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack3SwiftUIView()
+    }
+}
