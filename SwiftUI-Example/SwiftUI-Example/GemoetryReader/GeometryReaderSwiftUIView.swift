0ff47bfbbdbb7fb66129b7a4e1255104dc1ba242
醉看红尘这场梦 <15914115336@163.com>
2021-03-20 19:02:18 +0800

    GeometryReader2SwiftUIView

---
+++ SwiftUI-Example/SwiftUI-Example/GemoetryReader/GeometryReaderSwiftUIView.swift
@@ -0,0 +1,35 @@
+//
+//  GemoetryReaderSwiftUIView.swift
+//  SwiftUI-Example
+//
+//  Created by Cheney on 2021/3/20.
+//
+
+import SwiftUI
+
+struct GeometryReaderSwiftUIView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("GemoetryReader")
+                .font(.largeTitle)
+                .foregroundColor(.black)
+            
+            Text("Introducation")
+                .font(.title)
+                .foregroundColor(.gray)
+            
+            Text("GemometryReader is a container view that pushes out to fill up all available spce. You use it to help with positioning items within it.")
+                .font(.title)
+            
+            GeometryReader() { geometry in
+                
+            }.background(Color.pink)
+        }
+    }
+}
+
+struct GeometryReaderSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReaderSwiftUIView()
+    }
+}
--- SwiftUI-Example/SwiftUI-Example/Spacer/Spacer4SwiftUIView.swift
+++ SwiftUI-Example/SwiftUI-Example/Spacer/Spacer4SwiftUIView.swift
@@ -9,12 +9,54 @@ import SwiftUI
 
 struct Spacer4SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Spacer")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            
+            Text("Relative Spacing")
+                .font(.largeTitle)
+                .foregroundColor(.gray)
+            
+            VStack() {
+                Text("You can add more spacers to create relative spacing in comparision to other spacers.")
+                    .font(.title)
+                    .foregroundColor(.black)
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color(#colorLiteral(red: 0.9965897202, green: 0.8405602574, blue: 0.02881385945, alpha: 1)))
+            HStack(){
+                VStack(spacing: 5) {
+                    Spacer().frame(width: 5)
+                        .background(Color.blue)
+                    Text("33% Dowm")
+                        .font(.title)
+                    Spacer().frame(width: 5)
+                        .background(Color.blue)
+                    Spacer().frame(width: 5)
+                        .background(Color.blue)
+                }
+                
+                VStack(spacing: 5) {
+                    Spacer().frame(width: 5)
+                        .background(Color.blue)
+                    Spacer().frame(width: 5)
+                        .background(Color.blue)
+                    Spacer().frame(width: 5)
+                        .background(Color.blue)
+                    Text("75% Dowm")
+                        .font(.title)
+                    Spacer().frame(width: 5)
+                        .background(Color.blue)
+                }
+            }
+        }
     }
 }
 
 struct Spacer4SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        Spacer4SwiftUIView()
+        Spacer4SwiftUIView().preferredColorScheme(.dark)
     }
 }
