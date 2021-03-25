fae349a9dd1ba73945ad969ec509c28118adfc29
醉看红尘这场梦 <15914115336@163.com>
2021-03-25 12:13:39 +0800

    文件目录调整

---
+++ SwiftUI-Example/SwiftUI-Example/Classes/List/ListSwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  ListSwiftUIView.swift
+//  SwiftUI-Example
+//
+//  Created by Cheney on 2021/3/25.
+//
+
+import SwiftUI
+
+struct ListSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListSwiftUIView()
+    }
+}
