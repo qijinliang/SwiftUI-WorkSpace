22951795411c04eb5d6994ea5c3937963237844d
醉看红尘这场梦 <15914115336@163.com>
2021-04-22 21:54:35 +0800

    修改文字Project

--- SwiftUI-Example/SwiftUI-Example/AppDelegate.swift
+++ SwiftUI-Example/SwiftUI-Example/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.        
+        // Override point for customization after application launch.
         ATTrackingManager.requestTrackingAuthorization(completionHandler: { (status) in
             print("\(status)")
         })
