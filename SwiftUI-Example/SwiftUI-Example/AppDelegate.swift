070a2a53662f7c9da13420302eb3a8dd1f58f260
醉看红尘这场梦 <15914115336@163.com>
2021-04-23 15:19:25 +0800

    ATTrackingManager扩展

--- SwiftUI-Example/SwiftUI-Example/AppDelegate.swift
+++ SwiftUI-Example/SwiftUI-Example/AppDelegate.swift
@@ -10,31 +10,42 @@ import AppTrackingTransparency
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        ATTrackingManager.requestTrackingAuthorization(completionHandler: { (status) in
-            print("\(status)")
-        })
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { (status) in
+                if status == .authorized {
+                    print("statue--->\(status)")
+                } else if status == .denied {
+                    print("statue--->\(status)")
+                } else if status == .notDetermined {
+                    print("statue--->\(status)")
+                } else if status == .restricted {
+                    print("statue--->\(status)")
+                }
+            }
+        }
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
