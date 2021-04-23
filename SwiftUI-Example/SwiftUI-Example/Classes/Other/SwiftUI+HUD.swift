e5e56bd631c980a9048707d6997c6be8ea1dd689
醉看红尘这场梦 <15914115336@163.com>
2021-04-23 22:18:39 +0800

    SwiftUI+HUD自定提示

---
+++ SwiftUI-Example/SwiftUI-Example/Classes/Other/SwiftUI+HUD.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI+HUD.swift
+//  SwiftUI-Example
+//
+//  Created by Cheney on 2021/4/23.
+//
+
+import SwiftUI
+
+struct SwiftUI_HUD: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUI_HUD_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUI_HUD()
+    }
+}
