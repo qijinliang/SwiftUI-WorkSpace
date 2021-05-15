36c75030be192805661078dab49ddce34041f9c1
醉看红尘这场梦 <15914115336@163.com>
2021-05-15 17:13:25 +0800

    苹果官方实例

---
+++ SwiftUI-Scrumdinger/SwiftUI-Scrumdinger/SwiftUI_ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ScrumdingerApp.swift
+//  SwiftUI-Scrumdinger
+//
+//  Created by 金亮齐 on 2021/5/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
