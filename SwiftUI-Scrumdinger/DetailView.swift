5ef2ac9700563e0ec2674fac8aa722e8f8924c0d
醉看红尘这场梦 <15914115336@163.com>
2021-05-19 10:52:09 +0800

    Navigation and Modal Presentation Creating the Edit View

---
+++ SwiftUI-Scrumdinger/DetailView.swift
@@ -0,0 +1,52 @@
+//
+//  DetailView.swift
+//  SwiftUI-Scrumdinger
+//
+//  Created by Cheney on 2021/5/18.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("metting  info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                    .accessibilityLabel(Text("Start Meeting"))
+                
+                HStack() {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                
+                HStack() {
+                    Label("Color", systemImage: "paintpalette")
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(scrum.color)
+                }
+                .accessibilityElement(children: .ignore)
+            }
+            
+            Section(header: Text("attendees")) {
+                ForEach(scrum.attendees, id: \.self) { attendee in
+                    Label(attendee, systemImage: "person")
+                }
+                
+            }
+            
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(scrum: DailyScrum.data[0])
+    }
+}
