ee9ac7b0ef3881d8883eadf58035c413b276e7b4
醉看红尘这场梦 <15914115336@163.com>
2021-06-11 22:10:28 +0800

    添加画线

--- SwiftUI-Example/SwiftUI-Example/Animation/Path/Path2.swift
+++ SwiftUI-Example/SwiftUI-Example/Animation/Path/Path2.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct Path2: View {
     var body: some View {
         ZStack() {
+            
             Path() { path in
                 //MARK: 起始点坐标
                 path.move(to: CGPoint(x: 5, y: 5))
@@ -85,6 +86,64 @@ struct Path2: View {
                 
             }
             .stroke(Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)),lineWidth: 5)
+            
+ 
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 5, y: (width / 2) + 400))
+                path.addLine(to: CGPoint(x: 5, y: width - 100))
+                path.addLine(to: CGPoint(x: width - 5, y: width - 100))
+                path.addLine(to: CGPoint(x: width - 5, y: (width / 2) + 400))
+                
+                path.closeSubpath()
+                
+            }
+            .stroke(Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)),lineWidth: 5)
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 20, y: (width / 2) + 380))
+                path.addLine(to: CGPoint(x: 20, y: width - 80))
+                path.addLine(to: CGPoint(x: width - 20, y: width - 80))
+                path.addLine(to: CGPoint(x: width - 20, y: (width / 2) + 380))
+                
+                path.closeSubpath()
+                
+            }
+            .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)),lineWidth: 5)
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 35, y: (width / 2) + 360))
+                path.addLine(to: CGPoint(x: 35, y: width - 60))
+                path.addLine(to: CGPoint(x: width - 35, y: width - 60))
+                path.addLine(to: CGPoint(x: width - 35, y: (width / 2) + 360))
+                
+                path.closeSubpath()
+                
+            }
+            .stroke(Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)),lineWidth: 5)
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 50, y: (width / 2) + 340))
+                path.addLine(to: CGPoint(x: 50, y: width - 40))
+                path.addLine(to: CGPoint(x: width - 50, y: width - 40))
+                path.addLine(to: CGPoint(x: width - 50, y: (width / 2) + 340))
+                
+                path.closeSubpath()
+                
+            }
+            .stroke(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)),lineWidth: 5)
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 65, y: (width / 2) + 320))
+                path.addLine(to: CGPoint(x: 65, y: width - 20))
+                path.addLine(to: CGPoint(x: width - 65, y: width - 20))
+                path.addLine(to: CGPoint(x: width - 65, y: (width / 2) + 320))
+                
+                path.closeSubpath()
+                
+            }
+            .stroke(Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)),lineWidth: 5)
+
         }
     }
 }
