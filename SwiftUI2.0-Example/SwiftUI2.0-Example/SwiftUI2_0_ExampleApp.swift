963dfa152501486f143baee072cc35fc0a9d8631
醉看红尘这场梦 <15914115336@163.com>
2021-07-22 01:01:33 +0800

    更新

---
+++ SwiftUI2.0-Example/SwiftUI2.0-Example/SwiftUI2_0_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2_0_ExampleApp.swift
+//  SwiftUI2.0-Example
+//
+//  Created by 金亮齐 on 2021/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2_0_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
