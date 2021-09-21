203df6c273a6bc5d724ceab2317c9dbf69b32cfe
醉看红尘这场梦 <15914115336@163.com>
2021-09-21 16:32:13 +0800

    更新Xcode13

--- SwiftUI2.0-Example/Pods/Target Support Files/Pods-SwiftUI2.0-Example/Pods-SwiftUI2.0-Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftUI2_0_Example : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftUI2_0_Example
-@end
---
+++ SwiftUI3.0-Example/SwiftUI3.0-Example/SwiftUI3_0_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3_0_ExampleApp.swift
+//  SwiftUI3.0-Example
+//
+//  Created by 金亮齐 on 2021/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3_0_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
