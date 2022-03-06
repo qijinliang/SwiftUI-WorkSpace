d6e0d88e22ecb3e330a449c4c86b330de717b578
醉看红尘这场梦 <15914115336@163.com>
2022-03-06 10:34:23 +0800

    添加新项目

---
+++ SwiftUI-iOS15/SwiftUI-iOS15/SwiftUI_iOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iOS15App.swift
+//  SwiftUI-iOS15
+//
+//  Created by Cheney on 2022/3/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
