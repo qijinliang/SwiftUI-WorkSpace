8e6d4f97ecbd1a2afa56c7fd7694a79279e2d40f
醉看红尘这场梦 <15914115336@163.com>
2022-03-15 14:58:54 +0800

    完成登录页面

--- SwiftUI-iOS15/SwiftUI-iOS15/Extension/PreferencenKey.swift
+++ SwiftUI-iOS15/SwiftUI-iOS15/Extension/PreferencenKey.swift
@@ -14,4 +14,11 @@ struct ScrollPreferenceKey: PreferenceKey {
     }
 }
 
+struct CirclePreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
 
--- SwiftUI-iOS15/SwiftUI-iOS15/ProfileView/SignUpView.swift
+++ SwiftUI-iOS15/SwiftUI-iOS15/ProfileView/SignUpView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct SignUpView: View {
     
+    enum Field: Hashable {
+        case email
+        case password
+    }
+    
     @State var email = ""
     @State var password = ""
+    @FocusState var focusedField: Field?
+    @State var circleY: CGFloat = 120
+    @State var emailY: CGFloat = 0
+    @State var passwordY: CGFloat = 0
+    @State var circleColor: Color = .blue
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             Text("登录")
@@ -24,10 +35,23 @@ struct SignUpView: View {
                 .keyboardType(.emailAddress)
                 .autocapitalization(.none)
                 .disableAutocorrection(true)
+                .focused($focusedField, equals: .email)
+                .shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
+                .overlay(geometry)
+                .onPreferenceChange(CirclePreferenceKey.self) { value in
+                    emailY =  value
+                    circleY = value
+                }
             
             SecureField("密码",text: $password)
                 .inputStyle(icon: "lock")
                 .textContentType(.password)
+                .focused($focusedField, equals: .password)
+                .shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
+                .overlay(geometry)
+                .onPreferenceChange(CirclePreferenceKey.self) { value in
+                    passwordY =  value
+                }
             
             Button {} label: {
                 Text("创建用户")
@@ -59,12 +83,36 @@ struct SignUpView: View {
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .background(
+            Circle().fill(circleColor)
+                .frame(width: 68, height: 68)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .offset(y: circleY)
+        )
+        .coordinateSpace(name: "container")
         .strokeStyle(cornerRadius: 30)
         .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
         .padding(20)
         .background(
             Image("Blob 1").offset(x: 200, y: -100)
         )
+        .onChange(of: focusedField) { value in
+            withAnimation {
+                if value == .email {
+                    circleY = emailY
+                    circleColor = .blue
+                } else {
+                    circleY = passwordY
+                    circleColor = .red
+                }
+            }
+        }
+    }
+    
+    var geometry: some View {
+        GeometryReader { proxy in
+            Color.clear.preference(key: CirclePreferenceKey.self, value: proxy.frame(in: .named("container")).minY)
+        }
     }
 }
 
