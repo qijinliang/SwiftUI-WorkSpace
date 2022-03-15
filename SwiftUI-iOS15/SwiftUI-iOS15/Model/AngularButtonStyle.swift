e41aa6f2ae05d87037c233e765df495a214b381a
醉看红尘这场梦 <15914115336@163.com>
2022-03-15 09:30:46 +0800

    登录页面2

--- SwiftUI-iOS15/SwiftUI-iOS15/Model/AngularButtonStyle.swift
+++ SwiftUI-iOS15/SwiftUI-iOS15/Model/AngularButtonStyle.swift
@@ -9,12 +9,54 @@ import SwiftUI
 
 struct AngularButtonStyle: ButtonStyle {
     
+    @Environment(\.controlSize) var controlSize
+    
+    var extraPadding: CGFloat {
+        switch controlSize {
+        case .mini:
+            return 0
+        case .small:
+            return 0
+        case .regular:
+            return 4
+        case .large:
+            return 12
+        @unknown default:
+            return 0
+        }
+    }
+    
+    var cornerRadius: CGFloat {
+        switch controlSize {
+        case .mini:
+            return 12
+        case .small:
+            return 12
+        case .regular:
+            return 16
+        case .large:
+            return 20
+        @unknown default:
+            return 12
+        }
+    }
+    
     func makeBody(configuration: Configuration) -> some View {
+
         configuration.label
-            .padding(.horizontal, 10)
-            .padding(.vertical, 4)
-            .background(Color(.systemBackground))
-            .cornerRadius(20)
+            .padding(.horizontal, 10 + extraPadding)
+            .padding(.vertical, 4 + extraPadding)
+            .background(
+                RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                    .fill(.linearGradient(colors: [Color(.systemBackground),Color(.systemBackground).opacity(0.6)], startPoint: .top, endPoint: .bottom))
+                    .blendMode(.softLight)
+            )
+            .background(
+                RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                    .fill(.angularGradient(colors: [.pink,.purple,.blue,.pink], center: .center, startAngle: .degrees(-90), endAngle: .degrees(270)))
+                    .blur(radius: cornerRadius)
+            )
+            .strokeStyle(cornerRadius: cornerRadius)
     }
 }
 
--- SwiftUI-iOS15/SwiftUI-iOS15/ProfileView/SignUpView.swift
+++ SwiftUI-iOS15/SwiftUI-iOS15/ProfileView/SignUpView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @State var email = ""
+    @State var password = ""
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             Text("登录")
@@ -19,9 +22,29 @@ struct SignUpView: View {
                 Text("创建用户")
                     .frame(maxWidth: .infinity)
             }
-            .buttonStyle(.bordered)
+            .font(.headline)
+            .blendMode(.overlay)
+            .buttonStyle(.angular)
             .tint(.accentColor)
             .controlSize(.large)
+            
+            Group {
+                Text("点击")
+                + Text("需要查看用户协议").foregroundColor(.primary.opacity(0.7))
+                + Text("，**[禁止商业转载](https://github.com/qijinliang)**")
+                
+                Divider()
+                
+                HStack {
+                    Text("有账号？")
+                    Button {} label: {
+                        Text("点击登录")
+                    }
+                }
+            }
+            .font(.footnote)
+            .foregroundColor(.secondary)
+            .accentColor(.secondary)
         }
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
