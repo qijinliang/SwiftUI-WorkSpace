300ed79c3d15057fb8253479123967422240921a
醉看红尘这场梦 <15914115336@163.com>
2022-03-30 09:27:07 +0800

    个人中心2

--- SwiftUI-Discover/SwiftUI-Discover/User/UserDetailsView.swift
+++ SwiftUI-Discover/SwiftUI-Discover/User/UserDetailsView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+
+//let urlString = "https://travel.letsbuildthatapp.com/travel_discovery/user?id=0"
+
 struct UserDetailsView: View {
     
     let user: User
@@ -33,7 +36,99 @@ struct UserDetailsView: View {
                     Text("1233")
                 }
                 .font(.system(size: 12, weight: .regular))
-            }
+                
+                Text("YouTuber, Vlogger, Trvavel Create")
+                    .font(.system(size: 14, weight: .regular))
+                    .foregroundColor(Color(.lightGray))
+                
+                HStack(spacing: 12) {
+                    VStack {
+                        Text("59,394")
+                            .font(.system(size: 13, weight: .semibold))
+                        Text("Followers")
+                            .font(.system(size: 9, weight: .regular))
+                    }
+                    Spacer()
+                        .frame(width: 0.5, height: 12)
+                        .background(Color(.lightGray))
+                    
+                    VStack {
+                        Text("59,394")
+                            .font(.system(size: 13, weight: .semibold))
+                        Text("Followers")
+                            .font(.system(size: 9, weight: .regular))
+                    }
+                }
+                HStack(spacing: 12) {
+                    Button(action: {}, label: {
+                        HStack {
+                            Spacer()
+                            Text("Follow")
+                                .foregroundColor(.white)
+                            Spacer()
+                        }
+                        .padding(.vertical, 8)
+                        .background(Color.orange)
+                        .cornerRadius(100)
+                    })
+                    
+                    Button(action: {}, label: {
+                        HStack {
+                            Spacer()
+                            Text("Contact")
+                                .foregroundColor(.black)
+                            Spacer()
+                        }
+                        .padding(.vertical, 8)
+                        .background(Color(white: 0.9))
+                        .cornerRadius(100)
+                    })
+                }
+                .font(.system(size: 11, weight: .semibold))
+                
+                ForEach(0..<5, id: \.self) { num in
+                    VStack(alignment: .leading) {
+                        Image("japan")
+                            .resizable()
+                            .scaledToFill()
+                        
+                        HStack {
+                            Image("amy")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 34)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("here is my post title")
+                                    .font(.system(size: 14, weight: .semibold))
+                                Text("500K views")
+                                    .font(.system(size: 12, weight: .regular))
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                        .padding(.horizontal, 8)
+                        
+                        HStack {
+                            ForEach(0..<3, id: \.self) { num in
+                             Text("Traveling")
+                                    .foregroundColor(Color.blue)
+                                    .font(.system(size: 14, weight: .semibold))
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 4)
+                                    .background(Color(white: 0.9))
+                                    .cornerRadius(20)
+                            }
+                        }
+                        .padding(.bottom)
+                        .padding(.horizontal, 8)
+                    }
+                    .background(Color(white: 1))
+                    .cornerRadius(12)
+                    .shadow(color: .init(white: 0.8), radius: 5, x: 0, y: 4)
+                    
+                }
+            }.padding(.horizontal)
         }
         .navigationBarTitle(user.name, displayMode: .inline)
     }
