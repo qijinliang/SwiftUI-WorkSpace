f07dc81eebd59a02b6b6b9e2d472c014292f4763
醉看红尘这场梦 <15914115336@163.com>
2022-04-03 00:34:35 +0800

    添加新例子

---
+++ SwiftUI-Wallet/SwiftUI-Wallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Wallet
+//
+//  Created by 金亮齐 on 2022/4/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Wallet/SwiftUI-Wallet/SwiftUI_WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WalletApp.swift
+//  SwiftUI-Wallet
+//
+//  Created by 金亮齐 on 2022/4/3.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
