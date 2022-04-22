8c9c9f696e26974297aa2f13c6631f5ddab6fafe
醉看红尘这场梦 <15914115336@163.com>
2022-04-22 11:15:04 +0800

    视图扩充

--- SwiftUI3.0-Example/SwiftUI3.0-Example/Animation/Change/ChangeShowView.swift
+++ SwiftUI3.0-Example/SwiftUI3.0-Example/Animation/Change/ChangeShowView.swift
@@ -10,7 +10,73 @@ import SwiftUI
 struct ChangeShowView: View {
     var body: some View {
         Form() {
-            Section() {
+            Section(header: Text("Section1").font(.title3).fontWeight(.heavy)) {
+                NavigationLink(destination: Change()) {
+                    Text("Change")
+                }
+                
+                NavigationLink(destination: ChangeColor()) {
+                    Text("ChangeColor")
+                }
+                
+                NavigationLink(destination: ChangeOffset()) {
+                    Text("ChangeOffset")
+                }
+                
+                NavigationLink(destination: ChangeShow()) {
+                    Text("ChangeShow")
+                }
+                
+                NavigationLink(destination: ChangePoint()) {
+                    Text("ChangePoint")
+                }
+            }
+            
+            Section(header: Text("Section2").font(.title3).fontWeight(.heavy)) {
+                NavigationLink(destination: Change()) {
+                    Text("Change")
+                }
+                
+                NavigationLink(destination: ChangeColor()) {
+                    Text("ChangeColor")
+                }
+                
+                NavigationLink(destination: ChangeOffset()) {
+                    Text("ChangeOffset")
+                }
+                
+                NavigationLink(destination: ChangeShow()) {
+                    Text("ChangeShow")
+                }
+                
+                NavigationLink(destination: ChangePoint()) {
+                    Text("ChangePoint")
+                }
+            }
+            
+            Section(header: Text("Section3").font(.title3).fontWeight(.heavy)) {
+                NavigationLink(destination: Change()) {
+                    Text("Change")
+                }
+                
+                NavigationLink(destination: ChangeColor()) {
+                    Text("ChangeColor")
+                }
+                
+                NavigationLink(destination: ChangeOffset()) {
+                    Text("ChangeOffset")
+                }
+                
+                NavigationLink(destination: ChangeShow()) {
+                    Text("ChangeShow")
+                }
+                
+                NavigationLink(destination: ChangePoint()) {
+                    Text("ChangePoint")
+                }
+            }
+            
+            Section(header: Text("Section4").font(.title3).fontWeight(.heavy)) {
                 NavigationLink(destination: Change()) {
                     Text("Change")
                 }
