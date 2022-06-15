b1204c8aa76a129b873b51aa2ec0e94cd72c7d89
醉看红尘这场梦 <15914115336@163.com>
2022-06-15 09:50:43 +0800

    下拉选择

--- SwiftUI3.0-Example/SwiftUI3.0-Example/Classes/Form/Form4View.swift
+++ SwiftUI3.0-Example/SwiftUI3.0-Example/Classes/Form/Form4View.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct Form4View: View {
+    @State var isExpanded = Array(repeating: false, count: 4)
+    
+    private let FAQ = [
+        (
+            question: "问题一",
+            answer: "折叠一"
+        ),
+        (
+            question: "问题二",
+            answer: "折叠二"
+        ),
+        (
+            question: "问题三",
+            answer: "折叠三"
+        ),
+        (
+            question: "问题四",
+            answer: "折叠四"
+        )
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(FAQ.indices, id: \.self) { index in
+                DisclosureGroup (
+                    isExpanded: $isExpanded[index],
+                    
+                    content: {
+                        Text(FAQ[index].answer)
+                            .font(.body)
+                            .fontWeight(.light)
+                    },
+                    
+                    label: {
+                        Text(FAQ[index].question)
+                            .font(.body)
+                            .bold()
+                    }
+                ).padding()
+            }
+        }
     }
 }
 
